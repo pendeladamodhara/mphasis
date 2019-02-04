0b90989f854ee1b5d854d68ea35a8c1646363c2e
damodara <pendeladamodhara@gmail.com>
2019-02-05 00:10:14 +0530

    added dependency and properties

--- src/main/java/com/example/demo/SpringBootExampleApplication.java
+++ src/main/java/com/example/demo/SpringBootExampleApplication.java
@@ -2,22 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RequestMapping("/demo")
-@RestController
+@EnableJpaRepositories
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String example() {
-
-		return "Hello World";
-	}
 }
